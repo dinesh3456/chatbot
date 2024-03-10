e2c8de384f1047a93c58b11d2adbd616ba841eb7
dinesh3456 <dinesh2952002@gmail.com>
2024-03-10 22:10:47 +0530

    added the frontend code

---
+++ backend/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from fastapi import Request
+from fastapi.responses import JSONResponse
+
+
+app = FastAPI()
+
+
+@app.post("/")
+async def handle_request(request: Request):
+    payload = await request.json()
+
+    intent = payload['queryResult']['intent']['displayName']
+    parameters = payload['queryResult']['parameters']
+    output_contexts = payload['queryResult']['outputContexts']
+
+    if intent == "shirt":
+        return JSONResponse(content={
+            "fulfillmentText": f"Received =={intent}== in the backend"
+        })
